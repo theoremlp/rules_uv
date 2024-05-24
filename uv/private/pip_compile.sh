811182847c7ccaade2b701414fc7b23f0947ba09
Mark Elliot <123787712+mark-thm@users.noreply.github.com>
2024-05-24 16:35:43 -0400

    Read the Python version from toolchain (#49)
    Instead of running Python to emit its version, read the Python version from the toolchain in Starlark.

--- uv/private/pip_compile.sh
+++ uv/private/pip_compile.sh
@@ -5,6 +5,7 @@ set -euo pipefail
 UV="{{uv}}"
 PYTHON_PLATFORM="{{python_platform}}"
 RESOLVED_PYTHON="{{resolved_python}}"
+PYTHON_VERSION="{{python_version}}"
 REQUIREMENTS_IN="{{requirements_in}}"
 REQUIREMENTS_TXT="{{requirements_txt}}"
 LABEL="{{label}}"
@@ -14,16 +15,13 @@ RESOLVED_PYTHON_BIN="$(dirname "$RESOLVED_PYTHON")"
 # set resolved python to front of the path
 export PATH="$RESOLVED_PYTHON_BIN:$PATH"
 
-# get the version of python to hand to uv pip compile
-PYTHON_VERSION="$(python3 -c 'import sys; print(f"{sys.version_info.major}.{sys.version_info.minor}")')"
-
 $UV pip compile \
     --generate-hashes \
     --emit-index-url \
     --no-strip-extras \
     --custom-compile-command "bazel run $LABEL" \
-    --python-version=$PYTHON_VERSION \
+    --python-version="$PYTHON_VERSION" \
     $(echo $PYTHON_PLATFORM) \
-    -o $REQUIREMENTS_TXT \
-    $REQUIREMENTS_IN \
+    -o "$REQUIREMENTS_TXT" \
+    "$REQUIREMENTS_IN" \
     "$@"
