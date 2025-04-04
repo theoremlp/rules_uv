64147415e5bd8a225e8ab81f61768eac94d080ad
Timotheus Bachinger <timotheus.bachinger@checkmk.com>
2025-04-04 09:17:52 +0200

    Fix up for 4bb353089085
    site_packages_extra_files may contain a folder prefix which would not be copied.
    see examples/typical/BUILD.bazel

--- uv/private/sync_venv.sh
+++ uv/private/sync_venv.sh
@@ -35,7 +35,7 @@ if [ ! -z ${site_packages_extra_files+x} ]; then
   site_packages_dir=$(find "$BUILD_WORKSPACE_DIRECTORY/$target/lib" -type d -name 'site-packages')
   for file in "${site_packages_extra_files[@]}"; do
     cp "$file" "$site_packages_dir"/
-    chmod +w "$site_packages_dir"/"$file"
+    chmod +w "${site_packages_dir}/$(basename ${file})"
   done
 fi
 
